ca4614077729a39c695cd11ce334b297a72bceb0
Alessandra <alessandra@atplus.com.br>
2023-06-02 14:09:25 -0300

    arrumando a identação

--- views/site/contato/index.php
+++ views/site/contato/index.php
@@ -1,93 +1,90 @@
-    <section class="container">
-      <div class="my-5 text-center">
-        <span class="h6 d-block">POSSUI ALGUMA DÚVIDA?</span>
-        <h1 class="display-4 text-primary">Fale Conosco</h1>
+<section class="container">
+   <div class="my-5 text-center">
+      <span class="h6 d-block">POSSUI ALGUMA DÚVIDA?</span>
+      <h1 class="display-4 text-primary">Fale Conosco</h1>
+   </div>
+   <div class="row">
+      <div class="col-lg mb-5">
+         <form class="bg-light rounded p-4 box-shadow" id="contact-form">
+            <div class="form-group">
+               <label for="clienteNome">Nome</label>
+               <input type="text" class="form-control" id="clienteNome" name="nome">
+            </div>
+            <div class="form-group">
+               <label for="clienteEmail">Email</label>
+               <input type="email" class="form-control" id="clienteEmail" name="email">
+            </div>
+            <div class="form-group">
+               <label for="clientephone">Telefone</label>
+               <input type="phone" class="form-control" id="clientephone" name="phone">
+            </div>
+            <div class="form-group">
+               <label for="clienteMensagem">Mensagem</label>
+               <textarea id="clienteMensagem" class="form-control" rows="4" name="descricao"></textarea>
+            </div>
+         </form>
+         <button class="btn btn-primary" id="button-test">Enviar Mensagem</button>
       </div>
-      <div class="row">
-        <div class="col-lg mb-5">
-          <form class="bg-light rounded p-4 box-shadow" id="contact-form">
-            <div class="form-group">
-              <label for="clienteNome">Nome</label>
-              <input type="text" class="form-control" id="clienteNome" name="nome">
-            </div>
-            <div class="form-group">
-              <label for="clienteEmail">Email</label>
-              <input type="email" class="form-control" id="clienteEmail" name="email">
-            </div>
-            <div class="form-group">
-              <label for="clientephone">Telefone</label>
-              <input type="phone" class="form-control" id="clientephone" name="phone">
-            </div>
-            <div class="form-group">
-              <label for="clienteMensagem">Mensagem</label>
-              <textarea id="clienteMensagem" class="form-control" rows="4" name="descricao"></textarea>
-            </div>
-          </form>
-            <button class="btn btn-primary" id="button-test">Enviar Mensagem</button>
-        </div>
-        <div class="col-lg">
-          <h2 class="h6">NOSSO ENDEREÇO</h2>
-          <a href="#"><img class="img-fluid box-shadow rounded mb-4" src="<?php echo $url; ?>/assets/img/mapa.png" alt="Endereço da Empresa"></a>
-          
-          <h2 class="h6">DADOS DE CONTATO</h2>
-          <ul class="list-unstyled text-secondary">
+      <div class="col-lg">
+         <h2 class="h6">NOSSO ENDEREÇO</h2>
+         <a href="#"><img class="img-fluid box-shadow rounded mb-4" src="<?php echo $url; ?>/assets/img/mapa.png" alt="Endereço da Empresa"></a>
+         <h2 class="h6">DADOS DE CONTATO</h2>
+         <ul class="list-unstyled text-secondary">
             <li>caravan@caravan.com.br</li>
             <li>21 99999-9999</li>
             <li>De Seg. à Sex. das 8h às 18h</li>
-          </ul>
-          
-          <h2 class="h6">REDES SOCIAIS</h2>
-          <ul class="list-unstyled" style="max-width: 260px">
+         </ul>
+         <h2 class="h6">REDES SOCIAIS</h2>
+         <ul class="list-unstyled" style="max-width: 260px">
             <li><a class="btn btn-outline-secondary btn-sm btn-block mb-2" href="#" >Facebook</a></li>
             <li><a class="btn btn-outline-secondary btn-sm btn-block mb-2" href="#">Twitter</a></li>
             <li><a class="btn btn-outline-secondary btn-sm btn-block mb-2" href="#">Youtube</a></li>
-          </ul>
-        </div>
+         </ul>
       </div>
-    </section>
-    
-    <section class="container">
-      <div class="my-5 text-center">
-        <span class="h6 d-block">AINDA ESTÁ COM DÚVIDAS?</span>
-        <h2 class="display-4 text-primary">Perguntas Frequentes</h2>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6" id="perguntasFrequentes" data-children=".pergunta">
-          <div class="pergunta py-2">
+   </div>
+</section>
+<section class="container">
+   <div class="my-5 text-center">
+      <span class="h6 d-block">AINDA ESTÁ COM DÚVIDAS?</span>
+      <h2 class="display-4 text-primary">Perguntas Frequentes</h2>
+   </div>
+   <div class="row justify-content-center">
+      <div class="col-md-6" id="perguntasFrequentes" data-children=".pergunta">
+         <div class="pergunta py-2">
             <a class="lead" data-toggle="collapse" data-parent="#perguntasFrequentes" href="#pergunta1" aria-expanded="true" aria-controls="pergunta1">→ É possível cancelar a viagem?</a>
             <div id="pergunta1" class="collapse show" role="tabpanel">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
             </div>
-          </div>
-          <div class="dropdown-divider"></div>
-          <div class="pergunta py-2">
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
             <a class="lead" data-toggle="collapse" data-parent="#perguntasFrequentes" href="#pergunta2" aria-expanded="true" aria-controls="pergunta2">→ Qual a garantia que tenho se perder o voo?</a>
             <div id="pergunta2" class="collapse" role="tabpanel">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
             </div>
-          </div>
-          <div class="dropdown-divider"></div>
-          <div class="pergunta py-2">
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
             <a class="lead" data-toggle="collapse" data-parent="#perguntasFrequentes" href="#pergunta3" aria-expanded="true" aria-controls="pergunta3">→ Qual a principal forma de viagem?</a>
             <div id="pergunta3" class="collapse" role="tabpanel">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
             </div>
-          </div>
-          <div class="dropdown-divider"></div>
-          <div class="pergunta py-2">
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
             <a class="lead" data-toggle="collapse" data-parent="#perguntasFrequentes" href="#pergunta4" aria-expanded="true" aria-controls="pergunta4">→ Qual o melhor destino de viagem?</a>
             <div id="pergunta4" class="collapse" role="tabpanel">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
             </div>
-          </div>
-          <div class="dropdown-divider"></div>
-          <div class="pergunta py-2">
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
             <a class="lead" data-toggle="collapse" data-parent="#perguntasFrequentes" href="#pergunta5" aria-expanded="true" aria-controls="pergunta5">→ Vocês dão descontos para grupos grandes?</a>
             <div id="pergunta5" class="collapse" role="tabpanel">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
             </div>
-          </div>
-          <div class="dropdown-divider"></div>
-        </div>
+         </div>
+         <div class="dropdown-divider"></div>
       </div>
-    </section>
+   </div>
+</section>
--- views/site/home/index.php
+++ views/site/home/index.php
@@ -1,187 +1,153 @@
-<!-- <main>
-
-    <section class="banner" id="banner">
-
-        <h2>
-            Em construção
-        </h2>
-
-        <p>
-            Agora na versão com PHP 7
-        </p>
-
-        <p class="lead">
-               <a href="<?php echo $url; ?>/dashboard/login" class="btn btn-lg btn-success">Entrar</a>
-        </p>
-
-    </section>
-
-
-</main> -->
-
 <section class="container-fluid">
-    <div class="row bg-dark text-white">
-
+   <div class="row bg-dark text-white">
       <div class="col-lg-7 p-0">
-        <div id="carouselCidades" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselCidades" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselCidades" data-slide-to="1"></li>
-            <li data-target="#carouselCidades" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/california.jpg" alt="California">
-              <div class="carousel-caption">
-                <h3 class="display-4">California</h3>
-              </div>
+         <div id="carouselCidades" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#carouselCidades" data-slide-to="0" class="active"></li>
+               <li data-target="#carouselCidades" data-slide-to="1"></li>
+               <li data-target="#carouselCidades" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/california.jpg" alt="California">
+                  <div class="carousel-caption">
+                     <h3 class="display-4">California</h3>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/paris.jpg" alt="California">
+                  <div class="carousel-caption">
+                     <h3 class="display-4">Paris</h3>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/dublin.jpg" alt="California">
+                  <div class="carousel-caption">
+                     <h3 class="display-4">Dublin</h3>
+                  </div>
+               </div>
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/paris.jpg" alt="California">
-              <div class="carousel-caption">
-                <h3 class="display-4">Paris</h3>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="<?php echo $url; ?>/assets/img/dublin.jpg" alt="California">
-              <div class="carousel-caption">
-                <h3 class="display-4">Dublin</h3>
-              </div>
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselCidades" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselCidades" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Anterior</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselCidades" role="button" data-slide="next">
+            </a>
+            <a class="carousel-control-next" href="#carouselCidades" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Próximo</span>
-          </a>
-        </div>
+            </a>
+         </div>
       </div>
-
       <div class="col-lg-5 p-4 align-self-center">
-        <h1 class="display-4">Realize a viagem do seu sonho</h1>
-        <p class="lead">O melhor local para viajar é com a Caravan, mais de 5.000 excursões concluídas.</p>
+         <h1 class="display-4">Realize a viagem do seu sonho</h1>
+         <p class="lead">O melhor local para viajar é com a Caravan, mais de 5.000 excursões concluídas.</p>
       </div>
-
-    </div>
+   </div>
 </section>
-
 <section class="py-5 bg-light text-center">
-    <div class="container">
+   <div class="container">
       <div class="my-5">
-        <span class="h6 d-block">TUDO DE MELHOR PARA VOCÊ</span>
-        <h2 class="display-4 text-primary">Aproveite as Vantagens</h2>
+         <span class="h6 d-block">TUDO DE MELHOR PARA VOCÊ</span>
+         <h2 class="display-4 text-primary">Aproveite as Vantagens</h2>
       </div>
       <div class="row">
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/passaporte.svg" alt="Passaporte">
-          </div>
-          <h3>Documentos</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/passagens.svg" alt="Passagens">
-          </div>
-          <h3>Passagens</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/translado.svg" alt="Translado">
-          </div>
-          <h3>Translado</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/praias.svg" alt="Praias">
-          </div>
-          <h3>Praias</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/bagagens.svg" alt="Bagagens">
-          </div>
-          <h3>Bagagens</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
-        <div class="col-xl-4 col-md-6">
-          <div style="height: 100px;" class="d-flex justify-content-center">
-            <img src="<?php echo $url; ?>/assets/img/icones/turismo.svg" alt="Turismo">
-          </div>
-          <h3>Turismo</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-        </div>
-
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/passaporte.svg" alt="Passaporte">
+            </div>
+            <h3>Documentos</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/passagens.svg" alt="Passagens">
+            </div>
+            <h3>Passagens</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/translado.svg" alt="Translado">
+            </div>
+            <h3>Translado</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/praias.svg" alt="Praias">
+            </div>
+            <h3>Praias</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/bagagens.svg" alt="Bagagens">
+            </div>
+            <h3>Bagagens</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
+         <div class="col-xl-4 col-md-6">
+            <div style="height: 100px;" class="d-flex justify-content-center">
+               <img src="<?php echo $url; ?>/assets/img/icones/turismo.svg" alt="Turismo">
+            </div>
+            <h3>Turismo</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+         </div>
       </div>
-
       <a href="planos.html" class="btn btn-primary btn-lg mt-4">Comprar Plano</a>
-    </div>
+   </div>
 </section>
-
 <section id="home-quote" class="p-md-5">
-    <blockquote class="blockquote text-center text-white p-md-5 p-3">
+   <blockquote class="blockquote text-center text-white p-md-5 p-3">
       <p class="display-4"><em>"A felicidade não está na estrada que leva a algum lugar. A felicidade é a própria estrada."</em></p>
       <footer class="blockquote-footer text-white">Bob Dylan</footer>
-    </blockquote>
+   </blockquote>
 </section>
-
 <section class="container">
-    <div class="my-5 text-center">
+   <div class="my-5 text-center">
       <span class="h6 d-block">AINDA ESTÁ COM DÚVIDAS?</span>
       <h2 class="display-4 text-primary">Perguntas Frequentes</h2>
-    </div>
-    <div class="row justify-content-center">
+   </div>
+   <div class="row justify-content-center">
       <div class="col-md-6" id="perguntasFrequentes">
-        <div class="pergunta py-2">
-          <a class="lead" data-toggle="collapse" href="#pergunta1" aria-expanded="true" aria-controls="pergunta1">→ É possível cancelar a viagem?</a>
-          <div id="pergunta1" class="collapse show" data-parent="#perguntasFrequentes" role="tabpanel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
-          </div>
-        </div>
-        <div class="dropdown-divider"></div>
-        <div class="pergunta py-2">
-          <a class="lead" data-toggle="collapse" href="#pergunta2" aria-expanded="true" aria-controls="pergunta2">→ Qual a garantia que tenho se perder o voo?</a>
-          <div id="pergunta2" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
-          </div>
-        </div>
-        <div class="dropdown-divider"></div>
-        <div class="pergunta py-2">
-          <a class="lead" data-toggle="collapse" href="#pergunta3" aria-expanded="true" aria-controls="pergunta3">→ Qual a principal forma de viagem?</a>
-          <div id="pergunta3" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
-          </div>
-        </div>
-        <div class="dropdown-divider"></div>
-        <div class="pergunta py-2">
-          <a class="lead" data-toggle="collapse" href="#pergunta4" aria-expanded="true" aria-controls="pergunta4">→ Qual o melhor destino de viagem?</a>
-          <div id="pergunta4" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
-          </div>
-        </div>
-        <div class="dropdown-divider"></div>
-        <div class="pergunta py-2">
-          <a class="lead" data-toggle="collapse" href="#pergunta5" aria-expanded="true" aria-controls="pergunta5">→ Vocês dão descontos para grupos grandes?</a>
-          <div id="pergunta5" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
-          </div>
-        </div>
-        <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
+            <a class="lead" data-toggle="collapse" href="#pergunta1" aria-expanded="true" aria-controls="pergunta1">→ É possível cancelar a viagem?</a>
+            <div id="pergunta1" class="collapse show" data-parent="#perguntasFrequentes" role="tabpanel">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+            </div>
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
+            <a class="lead" data-toggle="collapse" href="#pergunta2" aria-expanded="true" aria-controls="pergunta2">→ Qual a garantia que tenho se perder o voo?</a>
+            <div id="pergunta2" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+            </div>
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
+            <a class="lead" data-toggle="collapse" href="#pergunta3" aria-expanded="true" aria-controls="pergunta3">→ Qual a principal forma de viagem?</a>
+            <div id="pergunta3" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+            </div>
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
+            <a class="lead" data-toggle="collapse" href="#pergunta4" aria-expanded="true" aria-controls="pergunta4">→ Qual o melhor destino de viagem?</a>
+            <div id="pergunta4" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+            </div>
+         </div>
+         <div class="dropdown-divider"></div>
+         <div class="pergunta py-2">
+            <a class="lead" data-toggle="collapse" href="#pergunta5" aria-expanded="true" aria-controls="pergunta5">→ Vocês dão descontos para grupos grandes?</a>
+            <div id="pergunta5" class="collapse" data-parent="#perguntasFrequentes" role="tabpanel">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium lorem non vestibulum scelerisque. Proin a vestibulum sem, eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.</p>
+            </div>
+         </div>
+         <div class="dropdown-divider"></div>
       </div>
-    </div>
-    <div class="text-center my-4">
+   </div>
+   <div class="text-center my-4">
       <p class="small m-1">FALE CONOSCO PARA MAIS DÚVIDAS</p>
       <a class="btn btn-primary btn-lg" href="<?php echo $url; ?>/contato">Entre em Contato</a>
-    </div>
+   </div>
 </section>
